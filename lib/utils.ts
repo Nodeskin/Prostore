ff0a23c573f2860170095258d7049094d7111e2c
nodeSkin <shittujamiu@gmail.com>
2025-07-26 04:23:38 +0100

    Loaded product from database and implement Zod validation and types for product

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+//Convert a Prisma Object to a JSON object
+export function ConvertToPlainObject<T>(data: T): T {
+  return JSON.parse(JSON.stringify(data));
+}
+
+//Format number with decomal placces
+
+export function formatNumberWithDecimal(num: number): string {
+ const [int, decimal] = num.toString().split('.');
+ return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int;
+
+}
---
+++ lib/validator.ts
@@ -0,0 +1,25 @@
+import {z} from 'zod';
+import { formatNumberWithDecimal } from './utils';
+//SChema for Inserting product data
+
+const currency = z
+  .string().refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+  'Price must have exactly two decimal places'
+)
+
+export const insertProductSchema = z.object({
+  id: z.string().uuid(),
+  name: z.string().min(3, "Name must be at least 3 characters").max(100),
+  slug: z.string().min(3, "Slug must be at least 3 characters").max(100),
+  category: z.string().min(3, "Category must be at least 3 characters").max(100),
+  brand: z.string().min(3, "Brand must be at least 3 characters").max(100),
+  description: z.string().min(3, "Description must be at least 3 characters").max(100),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, "At least one image is required"),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: currency,
+
+});
+
+export type Product = z.infer<typeof insertProductSchema>;
